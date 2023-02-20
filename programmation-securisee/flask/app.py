c33bf7aad67db251f983ac43d0f87264936f4436
Alessandro G <112400062+GTAlessandro@users.noreply.github.com>
2023-02-20 16:36:05 +0100

    Delete app.py

--- programmation-securisee/flask/app.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask #import library
-app = Flask(__name__) #instantiate new application
-
-@app.route('/') #this is a decorator (attach the following function to 'app.route')
-def index():
-    return render_template("index.html")
